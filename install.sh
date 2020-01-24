844cc44e1c46b84fdb2c2b863a847be4ded40fe9
Dennis Muensterer <dennismuensterer@gmail.com>
2020-01-24 17:31:25 +0100

    Refactor install script to enable flags for specific installs

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ LAUNCH_AGENTS_DIR=$HOME/Library/LaunchAgents
 PLIST_NAME=$LAUNCH_AGENTS_DIR/LocationChanger.plist
 APP_SUPPORT_DIR="$HOME/.locations" #/Library/Application Support/LocationChanger"
 
-sudo -v
 
-echo "Copying script and making it executable"
-sudo cp "$SCRIPT_NAME" "$INSTALL_DIR"
 
-sudo chmod +x ${SCRIPT_NAME}
+function copyScript() {
+  echo "Copying script and making it executable"
+  sudo chmod +x ${SCRIPT_NAME}
+  sudo cp "$SCRIPT_NAME" "$INSTALL_DIR"
+}
+
 
 # generate a default config file if it doesn't exists
 function copyConfig() {
@@ -19,6 +21,24 @@ function copyConfig() {
   echo "Copying Config"
   cp LocationChanger.conf "$APP_SUPPORT_DIR"
 }
+
+function copyPlist() {
+  if [ ! -e "${LAUNCH_AGENTS_DIR}/LocationChanger.plist" ] || [ "$1" == "-f" ]; then
+    mkdir -p "${LAUNCH_AGENTS_DIR}"
+    echo "Copying Plist"
+    cp LocationChanger.plist "$LAUNCH_AGENTS_DIR"
+  fi
+
+  launchctl load ${PLIST_NAME}
+
+}
+
+sudo -v
+if [[ "$1" == "-p" ]]; then
+  copyPlist
+  exit 0
+fi
+
 if [ -e "${APP_SUPPORT_DIR}/LocationChanger.conf" ]; then
   echo "Existing config found. Do you want to overwrite it? (y/n)"
   read reply
@@ -29,12 +49,6 @@ else
   copyConfig
 fi
 
-if [ ! -e "${LAUNCH_AGENTS_DIR}/LocationChanger.plist" ] || [ "$1" == "-f" ]; then
-  mkdir -p "${LAUNCH_AGENTS_DIR}"
-  echo "Copying Plist"
-  cp LocationChanger.plist "$LAUNCH_AGENTS_DIR"
-fi
-
-launchctl load ${PLIST_NAME}
+copyPlist
 
 echo "Installation complete."
